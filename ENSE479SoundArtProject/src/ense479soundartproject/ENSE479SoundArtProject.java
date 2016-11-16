021e0c8d532fee2c9beabcc91b59a166872ef1ca
OriginalMidas <clayton.brez@gmail.com>
2016-11-15 21:52:14 -0600

    Documentation

--- ENSE479SoundArtProject/src/ense479soundartproject/ENSE479SoundArtProject.java
+++ ENSE479SoundArtProject/src/ense479soundartproject/ENSE479SoundArtProject.java
@@ -1,26 +1,30 @@
 /*
- To change this license header, choose License Headers in Project Properties.
- To change this template file, choose Tools | Templates
- and open the template in the editor.
+This program will accept a .wave file that has a name corresponding to the soundFile String within this program. It will play it and then close it immediately after. 
  */
 package ense479soundartproject;
+import java.io.IOException;
+import java.net.URL;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 /**
 
  @author Dolan
  */
+
 public class ENSE479SoundArtProject
 {
-
     public static Mixer mixer;
     public static Clip clip;
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnsupportedAudioFileException
     {
+        
+        // this api accepts .WAV files nativly. 
         Mixer.Info[] mixInfoArray = AudioSystem.getMixerInfo();
         for (Mixer.Info info : mixInfoArray)
         {
@@ -28,6 +32,7 @@ public class ENSE479SoundArtProject
         }
         // setup the mixer
         mixer = AudioSystem.getMixer(mixInfoArray[0]);
+        
         // create a dataline in the format of a clip
         AudioFormat format = null;
         DataLine.Info dataInfo = new DataLine.Info(Clip.class, format);  
@@ -39,7 +44,47 @@ public class ENSE479SoundArtProject
         {
             e.printStackTrace();
         }
+        
         // put a sound file in the clip
+        try 
+        {
+            String soundFile = "Lunar Intro.wav";
+            URL soundURL = ENSE479SoundArtProject.class.getResource("/resources/" + soundFile);
+           AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+           clip.open(); //clip fully loads the file before playing it
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        catch(LineUnavailableException lue)
+        {
+            lue.printStackTrace();
+        }
+        catch (UnsupportedAudioFileException uafe)
+        {
+            uafe.printStackTrace();
+        }
+        
+        // play the clip
+        // new thread is opened with this call. The main thread terminates with this new thread opened.
+        clip.start();
+        
+        // to counteract the main thread terminating we perform a dowhile loop to work around this. 
+        // clipstart takes a few mS to start 
+        do
+        {
+            try
+            {
+                Thread.sleep(50);
+            } // have this thread sleep for 50ms
+            catch(InterruptedException ie)
+            {
+                ie.printStackTrace();
+            }
+        }
+        while(clip.isActive() == true);
+        
     }
     
 }
