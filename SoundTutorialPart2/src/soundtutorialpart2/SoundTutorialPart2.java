0bde1b084dbd7515a08684439060b3263b56b5f6
OriginalMidas <clayton.brez@gmail.com>
2016-11-15 21:54:02 -0600

    Created the project

---
+++ SoundTutorialPart2/src/soundtutorialpart2/SoundTutorialPart2.java
@@ -0,0 +1,27 @@
+/*
+Author: Clayton Brezinski
+Student ID: 200220989
+Notes:
+*/
+package soundtutorialpart2;
+
+/**
+ *
+ * @author Dolan
+ */
+public class SoundTutorialPart2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+/*
+Purpose:
+Arguments:
+Outputs:
+Notes:
+*/
