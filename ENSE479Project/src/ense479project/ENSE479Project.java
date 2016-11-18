55c0537039cc8f6d0129921cd6df2e86dad70677
OriginalMidas <clayton.brez@gmail.com>
2016-11-17 22:58:21 -0600

    update

--- ENSE479Project/src/ense479project/ENSE479Project.java
+++ ENSE479Project/src/ense479project/ENSE479Project.java
@@ -1,107 +1,100 @@
 /*
- Author: Clayton Brezinski
- Student ID: 200220989
- Notes: This program will receive microphone input from the user and will play it back for them. It will prompt them asking if they would like to save the audio
-        clip as is, or if they would like to slow the clip down by 50%. If they chose to slow it down, it will play it for them again and then save the file 
-        with the name that the user requests. 
- */
+Author: Clayton Brezinski
+Student ID: 200220989
+Notes:
+*/
 package ENSE479Project;
-
-import java.io.IOException;
-import javax.sound.sampled.AudioFileFormat;
+import java.io.ByteArrayOutputStream;
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
-import java.io.File;
-
 /**
-
- @author Dolan
+ *
+ * @author Dolan
  */
-public class ENSE479Project
-{
+public class ENSE479Project {
 
     /**
-    Ask the user how long they would like to record for -- set a hard cap to 10s
-    Ask do the recording, ask if they would like to record a different one. 
-    Give a dialogue to listen to the recording again or to save it, or to slow it down by 50%
-        Save - prompt the user to input a name to save the file as. 
+     * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        System.out.println("Starting Sound Test");
-        final File audioFile = new File("record.wav");
-        int timeToRecord = 10000;  
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
         try
         {
-            // PCM_Signed because we want to work with .wav file formats
-            // 44100 for the sample rate because that is the highest rate .wav can achieve
-            // 16 bit sample size
-            // 2 for stereo output, 1 is for mono
-            // 4 for frame size in bytes
-            // 44100 frame rate
-            // bigEndian format, we actually want littleEndian for .wav files
-            AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
-            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-            // check if the DataLine is supported with the audio system that we are working with on this computer
-            if (AudioSystem.isLineSupported(info) == false)
-            {
-                System.err.println("Dataline is not supported");
-                System.exit(0);
-            }
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            final SourceDataLine sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceLine.open();
+
+            info = new DataLine.Info(TargetDataLine.class, format);
             final TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(info);
-            targetLine.open();  // prepare the system resources for recording
+            targetLine.open();
 
-            System.out.println("Start recording");
-            targetLine.start(); // begin getting data from the microphone
+            final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-            // write the recording to a file. 
-            Thread thread = new Thread()
+            Thread sourceThread = new Thread()
             {
                 @Override
                 public void run()
                 {
-                    // make audioStream for the targetLine
-                    AudioInputStream audioStream = new AudioInputStream(targetLine);
-                    try
+                    sourceLine.start(); // play whateve is in the source's buffer.
+                    while (true)
                     {
-                        // write our recording to our audio file. as long as the target dataline gets info from the microphone.
-                        AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, audioFile);    
+                        sourceLine.write(out.toByteArray(), 0, out.size()); // or out.size()
                     }
-                    catch (IOException ioe)
-                    {
-                        ioe.printStackTrace();
-                    }
-                    System.out.println("Stopped Recording");
                 }
             };
-            // we need to thread in order to record because we need 1 thread actually recording and writing the data, and the other thread operating the 
-            // .sleep() timeout and then eventually closing the program
-            thread.start();
-            thread.sleep(timeToRecord); // 500s
-            targetLine.stop(); // stop the recording
-            targetLine.close(); //close the targetLine
+            // input stuff from the microphone
+            Thread targetThread = new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    targetLine.start();
+                    byte[] data = new byte[targetLine.getBufferSize() / 5];
+                    int readBytes;
+                    while (true)
+                    {
+                        // deposit information into the data buffer and hold the number of bytes written to the buffer.
+                        readBytes = targetLine.read(data, 0, data.length);
+                        out.write(data, 0, readBytes);
+                    }
+                }
+            };
+
+            targetThread.start();
+            System.out.println("Start recording");
+            Thread.sleep(5000);
+            targetLine.stop();
+            targetLine.close();
+
+            System.out.println("End recording");
+            System.out.println("Start playback");
+            sourceThread.start();
+            Thread.sleep(5000);
+            sourceLine.stop();
+            sourceLine.close();
+            System.out.println("End playback");
+//
         }
         catch (LineUnavailableException lue)
         {
             lue.printStackTrace();
         }
-
         catch (InterruptedException ie)
         {
             ie.printStackTrace();
         }
-
+        System.exit(0);
     }
 
 }
-
 /*
- Purpose:
- Arguments:
- Outputs:
- Notes:
- */
+Purpose:
+Arguments:
+Outputs:
+Notes:
+*/
