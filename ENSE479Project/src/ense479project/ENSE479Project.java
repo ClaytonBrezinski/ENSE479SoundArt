dab8aa430119c904eb2910f4da83c34fc1528c39
OriginalMidas <clayton.brez@gmail.com>
2016-11-17 22:24:19 -0600

    update for branch change

---
+++ ENSE479Project/src/ense479project/ENSE479Project.java
@@ -0,0 +1,27 @@
+/*
+Author: Clayton Brezinski
+Student ID: 200220989
+Notes:
+*/
+package ense479project;
+
+/**
+ *
+ * @author Dolan
+ */
+public class ENSE479Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+/*
+Purpose:
+Arguments:
+Outputs:
+Notes:
+*/
